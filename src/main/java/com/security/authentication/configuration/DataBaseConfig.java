640c9cfe7e6585994fa8bdb4e5d1523545c63c00
Caramel1004 <Caramel1004.js@gmail.com>
2024-03-10 21:59:53 +0900

    🔧 Add: H2 DB Configuration
    Add: H2 DB Configuration

---
+++ src/main/java/com/security/authentication/configuration/DataBaseConfig.java
@@ -0,0 +1,49 @@
+package com.security.authentication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DataBaseConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    /**
+     * UserDetailsManager extends UserDetailsService
+     * UserDetailsManager는 UserDetailsService interface를 상속받은 interface 입니다.
+     * */
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User
+                .withUsername("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
+    }
+}
